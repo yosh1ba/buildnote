5c5b712640f9c35bcffd4ada28d8984472c6f615
yosh1ba <hk.r8.718@gmail.com>
2021-04-03 23:46:59 +0900

    feat: 投稿用ディレクトリ変更/bioアイコン変更

--- src/components/bio.js
+++ src/components/bio.js
@@ -37,8 +37,8 @@ const Bio = () => {
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
         src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        width={100}
+        height={100}
         quality={95}
         alt="Profile picture"
       />
