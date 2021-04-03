6a106f5bc6c42005d3d643c22bd3df707eb7e267
yosh1ba <hk.r8.718@gmail.com>
2021-04-04 00:45:18 +0900

    feat: bio.js プロフィール修正

--- src/components/bio.js
+++ src/components/bio.js
@@ -32,24 +32,29 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile-pic.png"
-        width={100}
-        height={100}
-        quality={95}
-        alt="Profile picture"
-      />
+      <div>
+        <StaticImage
+          className="bio-avatar"
+          layout="fixed"
+          formats={["AUTO", "WEBP", "AVIF"]}
+          src="../images/profile-pic.png"
+          width={100}
+          height={100}
+          quality={95}
+          alt="Profile picture"
+        />
+        <p className="bio-name">{author.name}</p>
+      </div>
       {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            フォローお待ちしています。
-          </a>
-        </p>
+        <div>
+          <p>
+            医療機関向けシステムのエンジニアをしています。
+            フロントエンドが好きで、最近はJamstackに夢中です。
+            {/* <a href={`https://twitter.com/${social?.twitter || ``}`}>
+              フォローお待ちしています。
+            </a> */}
+          </p>
+        </div>
       )}
     </div>
   )
