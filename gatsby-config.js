bbea0b7f22fe4a6948c836399aa8ceeb1e167d7c
yosh1ba <hk.r8.718@gmail.com>
2021-04-05 22:22:06 +0900

    feat: Sass with CSS Modules導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,6 +131,7 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    "gatsby-plugin-sass",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
