bfb81de5bfd41e3768dd6bec24d904de3863092a
yosh1ba <hk.r8.718@gmail.com>
2021-04-05 12:00:08 +0900

    feat: Netlify-CMS導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,6 +131,8 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    // 2021-04-05 netlify-cms導入
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
