b56d04227a62e567ae460e8970661645e2cd6375
yosh1ba <yoshiba.driven@gmail.com>
2021-04-15 23:31:55 +0900

    feat: カテゴリ名によるアイキャッチ表示

--- src/components/post.js
+++ src/components/post.js
@@ -2,15 +2,15 @@ import * as React from "react"
 import {Link} from "gatsby"
 import * as styles from "./post.module.scss"
 import TagsList from "./tagsList"
+import Image from "./image"
 
 const Post = ({slug, title, date, tags}) => {
   return (
     <div key={slug} className={styles.wrapper}>
       <Link to={slug} className={styles.link} itemProp="url">
-      {/* TODO:サムネイルにsvgアイコンを表示する */}
         <div className={styles.thumbnail}>
           <span className={styles.thumbnailIcon}>
-            <img src="/static/6979a759583eabc63decbbc5411e167e/aab46/monitor.png" />
+            <Image filename="dev.png" />
           </span>
         </div>
      
