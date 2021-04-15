26861de28bcc0f0dddb455f15772f7e8f44ef099
yosh1ba <yoshiba.driven@gmail.com>
2021-04-15 22:38:30 +0900

    feat: アイキャッチ導入

--- src/components/post.js
+++ src/components/post.js
@@ -7,13 +7,13 @@ const Post = ({slug, title, date, tags}) => {
   return (
     <div key={slug} className={styles.wrapper}>
       <Link to={slug} className={styles.link} itemProp="url">
-      {/* TODO:サムネイルにsvgアイコンを表示する
+      {/* TODO:サムネイルにsvgアイコンを表示する */}
         <div className={styles.thumbnail}>
-          <span>
-            sample
+          <span className={styles.thumbnailIcon}>
+            <img src="/static/6979a759583eabc63decbbc5411e167e/aab46/monitor.png" />
           </span>
         </div>
-      */}
+     
         <div>
           <h3 itemProp="headline" className={styles.titleStyle}>{title}</h3>
           <p className={styles.date}>{date}</p>
