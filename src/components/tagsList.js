b5abdc23ce2ad616e2a8f08934bd17a215e08979
yosh1ba <yoshiba.driven@gmail.com>
2021-04-19 00:31:45 +0900

    fix: アイキャッチのレイアウトが崩れる問題対応

--- src/components/tagsList.js
+++ src/components/tagsList.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 import kebabCase from 'lodash/kebabCase';
 
 import * as Styles from './tagsList.module.scss';
+import { divide } from 'lodash-es';
 
 const Tag = ({ tag }) => (
   <Link className={Styles.link} to={`/tags/${kebabCase(tag)}/`}>
@@ -11,11 +12,14 @@ const Tag = ({ tag }) => (
 );
 
 const Tagslist = ({ tags }) => (
+  <div>
   <ul className={Styles.tags}>
     {(tags || []).map(tag => (
       <Tag key={tag} tag={tag} />
     ))}
   </ul>
+  </div>
+
 );
 
 export default Tagslist
