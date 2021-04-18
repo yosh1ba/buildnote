0c2cc17b967e9237a84441eb7897d334a7d4fcfe
yosh1ba <yoshiba.driven@gmail.com>
2021-04-19 00:12:45 +0900

    fix: アイキャッチのレイアウトが崩れる問題対応

--- src/components/post.js
+++ src/components/post.js
@@ -18,11 +18,11 @@ const Post = ({slug, title, date, tags}) => {
                 placeholder="blurred"
               />
         </div>
-        <div style={{width: `500px`}}>
+        <article>
           <h3 itemProp="headline" className={styles.titleStyle}>{title}</h3>
           <p className={styles.date}>{date}</p>
           <TagsList tags={tags} />
-        </div>
+        </article>
       </Link>
     </div>
   )
