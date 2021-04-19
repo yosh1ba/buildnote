fe3336c457b6f9c769520a7c9835d0a6dbd19fee
yosh1ba <yoshiba.driven@gmail.com>
2021-04-19 22:37:59 +0900

    fix: アイキャッチアイコン取得エラー

--- src/components/post.js
+++ src/components/post.js
@@ -24,7 +24,7 @@ const Post = ({slug, title, date, tags, category}) => {
     }  
   `)
 
-  const image = getImage(data.allFile.nodes.find(n => n.name === category)
+  const image = getImage(data.allFile.nodes.find(n => n.name === (category))
     .childrenImageSharp[0])
 
 
