a9eec77fb2f77294e076dacd162e4a15b8d7e457
yosh1ba <yoshiba.driven@gmail.com>
2021-04-22 23:33:55 +0900

    feat: githubの草を表示させる機能を実装。ただし表示はオフにしておく。

---
+++ src/components/github.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const GitHub = () => {
+  return (
+    <StaticImage
+      src="https://grass-graph.moshimo.works/images/yosh1ba.png?background=none"
+      alt="GitHub"
+      placeholder="blurred"
+      layout="fullWidth"
+    />
+  )
+}
+
+export default GitHub;
